0c094a77ecbcf733f3c16b6252abe887158ac306
josesoco08 <167309570+josesoco08@users.noreply.github.com>
2024-11-18 00:40:50 -0300

    Actualizar router.php
    Agregó la autenticación

--- Tpe-web-p3/router.php
+++ Tpe-web-p3/router.php
@@ -2,14 +2,14 @@
 
 require_once 'libs/router.php';
 require_once 'app/controllers/ProductController.php';
-
+requiere_once 'app/middlewares.php';
 
 // base_url para direcciones y base tag
 define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
 
+
 $router = new Router();
-
-
+$router->addMiddleware(new JWTAuthMiddleware());
 // Rutas sin autenticación
 
 //                  endpoint         |  verbo    |     controller         |    método
@@ -18,5 +18,5 @@ $router->addRoute('product/:id'      ,   'GET'   , 'ProductController'    , 'get
 $router->addRoute('product'          ,   'POST'  , 'ProductController'    , 'newProduct');
 $router->addRoute('product/:id'      ,   'PUT'   , 'ProductController'    , 'updateProduct');
 $router->addRoute('product/:id'      , 'DELETE'  , 'ProductController'    , 'deleteProduct');
-
+$router->addRoute('usuarios/token', 'GET','UserApiController','getToken');
 $router->route($_GET['resource'], $_SERVER['REQUEST_METHOD']);
